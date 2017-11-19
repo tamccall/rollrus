b2c1c845de2c6758e104ce6e9c8d63ea847f6812
Andy McCall <andymc903@gmail.com>
2017-11-19 12:52:24 -0800

    init hook in benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -4,31 +4,20 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
-	"io"
 
 	"github.com/sirupsen/logrus"
 )
 
-var vanillaLogger *logrus.Logger
-var rollrusLogger *logrus.Logger
-var rollrusCloser io.Closer
 var token string
 
 func init() {
 	token = os.Getenv("ROLLBAR_TOKEN")
-
-	vanillaLogger = logrus.New()
-	rollrusLogger = logrus.New()
-
-	vanillaLogger.Out = ioutil.Discard
-	rollrusLogger.Out = ioutil.Discard
-
-	rollrus := NewHook(token, "test")
-	rollrusLogger.AddHook(rollrus)
-	rollrusCloser = rollrus
 }
 
 func BenchmarkVanillaLogger(b *testing.B) {
+	vanillaLogger := logrus.New()
+	vanillaLogger.Out = ioutil.Discard
+
 	for i := 0; i < b.N; i++ {
 		vanillaLogger.Error("test")
 	}
@@ -39,8 +28,14 @@ func BenchmarkRollrusLogger(b *testing.B) {
 		b.Skip("Could not get rollbar token")
 	}
 
+	rollrusLogger := logrus.New()
+	rollrusLogger.Out = ioutil.Discard
+	rollrus := NewHook(token, "test")
+	rollrusLogger.AddHook(rollrus)
+
 	for i := 0; i < b.N; i++ {
 		rollrusLogger.Error("test")
 	}
-	rollrusCloser.Close()
+
+	rollrus.Close()
 }
