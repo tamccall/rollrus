4ef17f0c84a4cc636bb3fd010052d3a769e3b378
Andy McCall (Sharon) <andymc@justin.tv>
2017-12-13 14:35:09 -0800

    Added test for channel buffer

---
+++ buffer/channel/buffer_test.go
@@ -0,0 +1,30 @@
+package channel
+
+import (
+	"testing"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+)
+
+func TestBuffer(t *testing.T) {
+	dummyLogger := logrus.New()
+	dummyLogger.Out = ioutil.Discard
+
+	b := NewBuffer(10)
+	for i := 0; i < 4; i++ {
+		b.Push(logrus.NewEntry(dummyLogger))
+	}
+
+	outCount := 0
+	for b.Next()  {
+		if outCount == 2 {
+			b.Close()
+		}
+
+		outCount++
+	}
+
+	if outCount != 4 {
+		t.Fatalf("Did not recieve all events from queue. Got %d expected %d", outCount, 4)
+	}
+}
