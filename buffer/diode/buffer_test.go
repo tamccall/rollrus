d3ce71636351731010a9e31867e80c47bb43fba1
Andy McCall (Sharon) <andymc@justin.tv>
2017-12-13 15:07:29 -0800

    Added diode test

---
+++ buffer/diode/buffer_test.go
@@ -0,0 +1,55 @@
+package diode
+
+import (
+	"testing"
+	"io/ioutil"
+	"github.com/sirupsen/logrus"
+)
+
+func TestBuffer(t *testing.T) {
+	dummyLogger := logrus.New()
+	dummyLogger.Out = ioutil.Discard
+
+	b := NewBuffer(10)
+	defer b.Close()
+
+	for i := 0; i < 4; i++ {
+		entry := logrus.NewEntry(dummyLogger).WithField("value", i)
+		b.Push(entry)
+	}
+
+	values := make([]int, 4)
+	i := 0
+	for b.Next()  {
+		entry := b.Value()
+		value, ok := entry.Data["value"]
+
+		if !ok {
+			t.Fatal("entry is missing value",)
+		}
+
+		v, ok := value.(int)
+
+		if !ok {
+			t.Fatal("entry was not a int",)
+		}
+
+		if i == 3 {
+			b.Close()
+		}
+
+		values[i] = v
+		i++
+	}
+
+	if len(values) != 4 {
+		t.Fatal("did not contain all entries")
+	}
+
+	for i, value := range values {
+		if value != i {
+			t.Fatalf("Value was not what we expected. Expected %d was %d", i , value)
+		}
+	}
+
+}
