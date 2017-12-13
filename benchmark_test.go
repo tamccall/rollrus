e4ff6881fd386944d32b766e8092f84eb045d418
Andy McCall <andymc903@gmail.com>
2017-12-13 00:07:03 -0800

    Added diode buffer

--- benchmark_test.go
+++ benchmark_test.go
@@ -4,7 +4,13 @@ import (
 	"io/ioutil"
 	"testing"
 
+	"github.com/benjamindow/rollrus/buffer/diode"
 	"github.com/sirupsen/logrus"
+)
+
+const (
+	env      = "test"
+	errorMSG = "test"
 )
 
 func BenchmarkVanillaLogger(b *testing.B) {
@@ -12,23 +18,45 @@ func BenchmarkVanillaLogger(b *testing.B) {
 	vanillaLogger.Out = ioutil.Discard
 
 	for i := 0; i < b.N; i++ {
-		vanillaLogger.Error("test")
+		vanillaLogger.Error(errorMSG)
 	}
 }
 
-func BenchmarkRollrusLogger(b *testing.B) {
+func BenchmarkWithChannelBuffer(b *testing.B) {
 	if token == "" {
 		b.Skip("Could not get rollbar token")
 	}
 
 	rollrusLogger := logrus.New()
 	rollrusLogger.Out = ioutil.Discard
-	hook := NewHook(token, "test")
+	hook := NewHook(token, env)
 	defer hook.Close()
 
 	rollrusLogger.AddHook(hook)
 
 	for i := 0; i < b.N; i++ {
-		rollrusLogger.Error("test")
+		rollrusLogger.Error(errorMSG)
+	}
+}
+
+func BenchmarkWithDiodeBuffer(b *testing.B) {
+	if token == "" {
+		b.Skip("Could not get rollbar token")
+	}
+
+	rollrusLogger := logrus.New()
+	rollrusLogger.Out = ioutil.Discard
+
+	//TODO: probably should rename this. Maybe drop the new hook for levels since it takes a config now
+
+	hook := NewHookForLevels(token, env, RollrusConfig{
+		Buffer: diode.NewBuffer(defaultBufferSize),
+	})
+	defer hook.Close()
+
+	rollrusLogger.AddHook(hook)
+
+	for i := 0; i < b.N; i++ {
+		rollrusLogger.Error(errorMSG)
 	}
 }
--- buffer/diode/buffer.go
+++ buffer/diode/buffer.go
@@ -1,1 +1,69 @@
 package diode
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/cloudfoundry/go-diodes"
+	"github.com/sirupsen/logrus"
+)
+
+func NewBuffer(size int) *Buffer {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	alerter := func(missed int) {
+		fmt.Fprintf(os.Stderr, "Overwrote %d entries", missed)
+	}
+
+	diode := diodes.NewManyToOne(size, diodes.AlertFunc(alerter))
+	waiter := diodes.NewWaiter(diode, diodes.WithWaiterContext(ctx))
+
+	return &Buffer{
+		close:  cancel,
+		closed: ctx.Done(),
+		waiter: waiter,
+	}
+}
+
+type Buffer struct {
+	waiter *diodes.Waiter
+	close  context.CancelFunc
+	closed <-chan struct{}
+}
+
+func (c *Buffer) Close() error {
+	c.close()
+	return nil
+}
+
+func (c *Buffer) Next() bool {
+	select {
+	case <-c.closed:
+		return false
+	default:
+		return true
+	}
+}
+
+func (c *Buffer) Value() *logrus.Entry {
+	val := c.waiter.Next()
+
+	if val == nil {
+		dummyLogger := logrus.New()
+		dummyLogger.Out = ioutil.Discard
+		return logrus.NewEntry(dummyLogger)
+	}
+
+	return (*logrus.Entry)(val)
+}
+
+func (c *Buffer) Push(entry *logrus.Entry) {
+	select {
+	case <-c.closed:
+		return
+	default:
+		c.waiter.Set(diodes.GenericDataType(entry))
+	}
+}
