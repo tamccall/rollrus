44a06fc30f9c31dfe60664192a722f5d4978d7f3
Andy McCall <andymc903@gmail.com>
2019-12-18 19:19:25 -0800

    Added code from benjamindow rollrus

---
+++ benchmark_test.go
@@ -0,0 +1,60 @@
+// +build go1.7
+
+package rollrus
+
+import (
+	"github.com/tamccall/rollrus/buffer/diode"
+	"io/ioutil"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	env      = "test"
+	errorMSG = "test"
+)
+
+func BenchmarkVanillaLogger(b *testing.B) {
+	vanillaLogger := logrus.New()
+	vanillaLogger.Out = ioutil.Discard
+
+	runBenchmark(b, vanillaLogger)
+}
+
+func BenchmarkWithChannelBuffer(b *testing.B) {
+	if token == "" {
+		b.Skip("Could not get rollbar token")
+	}
+
+	rollrusLogger := logrus.New()
+	rollrusLogger.Out = ioutil.Discard
+	hook := NewHook(token, env)
+	defer hook.Close()
+
+	rollrusLogger.AddHook(hook)
+
+	runBenchmark(b, rollrusLogger)
+}
+
+func BenchmarkWithDiodeBuffer(b *testing.B) {
+	if token == "" {
+		b.Skip("Could not get rollbar token")
+	}
+
+	rollrusLogger := logrus.New()
+	rollrusLogger.Out = ioutil.Discard
+
+	hook := NewHook(token, env, WithBuffer(diode.NewBuffer(defaultBufferSize)))
+	defer hook.Close()
+
+	rollrusLogger.AddHook(hook)
+
+	runBenchmark(b, rollrusLogger)
+}
+
+func runBenchmark(b *testing.B, rollrusLogger *logrus.Logger) {
+	for i := 0; i < b.N; i++ {
+		rollrusLogger.Error(errorMSG)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
-module github.com/heroku/rollrus
+module github.com/tamccall/rollrus
 
 require (
+	github.com/cloudfoundry/go-diodes v0.0.0-20190809170250-f77fb823c7ee // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/pkg/errors v0.8.2-0.20190227000051-27936f6d90f9
 	github.com/rollbar/rollbar-go v1.0.2
